4a1cd88a53a8c99d7df216c87ac43ee550bc80df
Blu <plush.gamer.456@gmail.com>
2022-11-04 23:55:55 -0400

    Slight overhaul
    Made it work with more than just NodeJS apps (untested, but should in theory)
    Also added Krogdor icon (important!)

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
 const print = console.log
 const internalIp = require('internal-ip')
 const path = require('path')
-const { spawn } = require('node:child_process')
+const { spawn, exec } = require('node:child_process')
 const ini = require('multi-ini')
 const fs = require('fs')
+const util = require('node:util')
+const asyncExec = (cmd, opts) => {
+	return new Promise((res, rej) => {
+		var proc = exec(cmd, opts)
+		print(`NEW PROCESS [ ${cmd} ]`)
+		proc.stdout.on('data', data => {
+			print(data)
+		})
+		proc.on('exit', (code) => {
+			// print(`${cmd}: ${code}`)
+			res(code)
+		})
+	})
+}
+const asyncMultiExec = (cmds, opts) => {
+	return new Promise((res, rej) => {
+		var codes = []
+		var count = 0
+		cmds.forEach(async cmd => {
+			var code = await asyncExec(cmd, opts)
+			codes.push(code)
+			count++
+			if (count == cmds.length) {
+				res(codes)
+			}
+		})
+	})
+}
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    historySize: 30,
+    prompt: '$ '
+})		
 
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
-const APPS_PATH = `${__dirname}/apps.ini`
-
-if (!fs.existsSync(APPS_PATH)) { throw new Error("apps.ini Not Found") }
-
-const apps_config = ini.read(APPS_PATH)
-
-const ROOT = (apps_config.root || "..")
-
-var node_apps = Object.keys(apps_config).map(node_app => {
-	let obj = apps_config[node_app]
-	obj.name = node_app
-	return obj
-})
-
-var processes = {}
-var debug = []
-
-var outs = {}
-var counts = {}
-var status = {}
-
-node_apps.forEach(node_app => {
-	if (outs[node_app.name] == null) {
-		outs[node_app.name] = []
-		debug[node_app.name] = {}
-		debug[node_app.name].error = []
-		debug[node_app.name].crash = []
-		counts[node_app.name] = {}
-		counts[node_app.name].error = 0
-		counts[node_app.name].crash = 0
-		status[node_app.name] = node_app.status
-	}
-	if (node_app.status == "ON") { app_spawn(node_app) }
-})
 
 Array.prototype.remove = function (index) {
     if (index > -1 && index < this.length-1) {
@@ -52,61 +52,155 @@ Array.prototype.remove = function (index) {
     }
 }
 
-function app_spawn(node_app) {
-	let p = spawn('node', [`${ROOT}/${node_app.package}/${node_app.entry}`])
+const APPS_PATH = `${__dirname}/apps.ini`
+const PLATS_PATH = `${__dirname}/plats.ini`
+
+if (!fs.existsSync(APPS_PATH)) { throw new Error("apps.ini Not Found") }
+	if (!fs.existsSync(PLATS_PATH)) { throw new Error("plats.ini Not Found") }
+
+const apps_config = ini.read(APPS_PATH)
+const plats_config = ini.read(PLATS_PATH, {nested_section_names: true})
+
+const ROOT = (apps_config.root || "..")
+
+var processes = {}
+var debug = []
+
+var outs = {}
+var counts = {}
+var status = {}
+
+var platforms = plats_config
+Object.keys(platforms).forEach(name => {
+	platforms[name].update_cmds = Object.values(platforms[name].update_cmds)
+})
+
+var bluto_apps = Object.keys(apps_config).map(bluto_app => {
+	let obj = apps_config[bluto_app]
+	obj.name = bluto_app
+	obj.dirname = obj.git.split("/")[4].split(".")[0]
+
+	outs[bluto_app] = []
+	debug[bluto_app] = {}
+	debug[bluto_app].error = []
+	debug[bluto_app].crash = []
+	counts[bluto_app] = {}
+	counts[bluto_app].error = 0
+	counts[bluto_app].crash = 0
+	status[bluto_app] = obj.status
+
+	return obj
+})
+
+// Startup:
+// ******************************
+function init() {
+	bluto_apps.forEach(bluto_app => {
+		if (bluto_app.status == "ON") { app_spawn(bluto_app) }
+	})
+}
+
+async function update_repos() {
+	return new Promise((res, rej) => {
+		var app_count = bluto_apps.length
+		var updated = 0
+		bluto_apps.forEach(async (bluto_app, ind) => {
+			var APP_PATH = `${ROOT}/${bluto_app.dirname}/`
+			if (fs.existsSync(APP_PATH)) {
+				await asyncExec("git reset --hard", {cwd: APP_PATH})
+				await asyncExec("git pull", {cwd: APP_PATH})
+			} else {
+				await asyncExec(`git clone ${bluto_app.git}`, {cwd: ROOT})
+			}
+			await asyncMultiExec(platforms[bluto_app.plat].update_cmds, {cwd: APP_PATH})
+			updated++
+			if (updated == app_count) {
+				print("Done updating!")
+				res("done!")
+			}
+		})
+	})
+}
+
+function preinit_prompt() {
+	rl.question("Checks for updates? [Y/N] ", async function (answer) {
+		var answer = answer.toUpperCase()
+		if (["Y", "N"].includes(answer)) {
+			if (answer == "Y") {
+				await update_repos()
+			}
+			init()
+			init_webserver()
+		} else {
+			preinit_prompt()
+		}
+	})
+}
+
+preinit_prompt()
+// ******************************
+
+// Main Logic:
+// ******************************
+function app_spawn(bluto_app) {
+	var args = bluto_app.run.split(" ")
+	var cmd = args.shift()
+
+	// print(cmd, args, {cwd: `${ROOT}/${bluto_app.dirname}/`})
+	let p = spawn(cmd, args, {cwd: `${ROOT}/${bluto_app.dirname}/`})
 	p.stdout.on('data', data => {
-		print(`[ ${node_app.name} ] ${data}`)
-		outs[node_app.name].push(data.toString())
-		io.sockets.emit('data', node_app.name, data.toString(), "normal")
+		print(`[ ${bluto_app.name} ] ${data}`)
+		outs[bluto_app.name].push(data.toString())
+		io.sockets.emit('data', bluto_app.name, data.toString(), "normal")
 	})
 	p.stderr.on(`data`, data => {
-		print(`[ ${node_app.name} ] ${data}`)
-		outs[node_app.name].push(data.toString())
-		counts[node_app.name].error++
-		debug[node_app.name].error.push({
+		print(`[ ${bluto_app.name} ] ${data}`)
+		outs[bluto_app.name].push(data.toString())
+		counts[bluto_app.name].error++
+		debug[bluto_app.name].error.push({
 			text: data.toString(),
 			time: Date.now()
 		})
-		io.sockets.emit('data', node_app.name, data.toString(), "error")
+		io.sockets.emit('data', bluto_app.name, data.toString(), "error")
 	})
 	p.on('error', (err) => {
-		print(`[ ${node_app.name} ] ${err}`)
-		outs[node_app.name].push(err.toString())
-		counts[node_app.name].error++
-		debug[node_app.name].error.push({
+		print(`[ ${bluto_app.name} ] ${err}`)
+		outs[bluto_app.name].push(err.toString())
+		counts[bluto_app.name].error++
+		debug[bluto_app.name].error.push({
 			text: err.toString(),
 			time: Date.now()
 		})
-		io.sockets.emit('data', node_app.name, err.toString(), "error")
+		io.sockets.emit('data', bluto_app.name, err.toString(), "error")
 	})
 	p.on('exit', (code, str) => {
 		if (code != null) {
 			let text = `Exited with code: ${code}\n\n\n[ SYSTEM ] Restarting app...\n\n\n`
-			print(`[ ${node_app.name} ] ${text}`)
-			outs[node_app.name].push(text)
-			counts[node_app.name].crash++
-			debug[node_app.name].crash.push({
+			print(`[ ${bluto_app.name} ] ${text}`)
+			outs[bluto_app.name].push(text)
+			counts[bluto_app.name].crash++
+			debug[bluto_app.name].crash.push({
 				text: `Exited with code: ${code}`,
 				time: Date.now()
 			})
-			io.sockets.emit('data', node_app.name, text, "exit")
-			app_spawn(node_app)
+			io.sockets.emit('data', bluto_app.name, text, "exit")
+			app_spawn(bluto_app)
 		}
 	})
-	processes[node_app.name] = p
+	processes[bluto_app.name] = p
 }
 
 function send_line(app_name, line) {
 	let p = processes[app_name]
 	p.stdin.cork()
-	p.stdin.write(line + "\n")
+	p.stdin.write(line)
 	p.stdin.uncork()
 }
 
 io.on('connection', socket => {
 	print(`[ SYSTEM ] + NEW SOCKET CONNECTION`)
 
-	socket.emit('init', node_apps, outs, counts, status)
+	socket.emit('init', bluto_apps, outs, counts, status)
 
 	socket.on('line', send_line)
 
@@ -130,7 +224,7 @@ io.on('connection', socket => {
 			socket.emit('data', app_name, text, "normal")
 		} else {
 			status[app_name] = 'ON'
-			app_spawn(node_apps[pid])
+			app_spawn(bluto_apps[pid])
 		}
 		socket.emit('status_change', app_name, status[app_name])
 	})
@@ -146,6 +240,10 @@ app.get('/debug', (req, res) => {
   res.sendFile('/debug.html', {root: path.join(__dirname, 'website')});
 })
 
-server.listen(2026, internalIp.v4.sync(), () => {
-	print("Lisening on port 2026")
-})
+function init_webserver() {
+	server.listen(2026, internalIp.v4.sync(), () => {
+		print("Lisening on port 2026")
+	})
+}
+// ******************************
+// t( / U)t < [ Hi weird people! ]
